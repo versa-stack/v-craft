9a53476d90a0d01e18a2dbd95eb2049b7cda6187
khusseini <khusseini@users.noreply.github.com>
2024-09-26 06:26:23 +0000

    deploy: 3481b309d636863862f54a0eb1e9ce0dd66f2144

---
+++ develop/assets/chunks/viewer.vue_vue_type_script_setup_true_lang.BEug9fnF.js
@@ -0,0 +1,1 @@
+import{u as o}from"./theme.DhiTGzEC.js";import{r}from"./resolvermap.BgM6vmYh.js";import{d as t,M as s,o as a,g as n,N as c,f as m}from"./framework.CyFIEpWX.js";const p={class:"content"},C=t({__name:"viewer",setup(_){return o().disable(),(l,d)=>{const e=s("CraftFrame");return a(),n("div",p,[c(e,{resolverMap:m(r)},null,8,["resolverMap"])])}}});export{C as _};
