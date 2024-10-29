de19152c093b6e92d8eb6c48a00521f129ccd6d0
K. Husseini <kmhusseini@gmail.com>
2024-10-29 06:51:12 -0700

    docs: Added more initial content with event handling example.

--- docs/components/demo/editor-app/editor.vue
+++ docs/components/demo/editor-app/editor.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="w-full mb-10">
     <label for="editorEnabled">Preview Content: </label>
-    <input id="editorEnabled" type="checkbox" name="editorEnabled" v-model="previewContent" />
+    <input
+      id="editorEnabled"
+      type="checkbox"
+      name="editorEnabled"
+      v-model="previewContent"
+    />
   </div>
   <CraftEditor :config="config">
     <CraftCanvas component="CraftComponentSimpleContainer" />
@@ -11,15 +16,76 @@
 import { CraftEditorConfig, useEditor } from "@versa-stack/v-craft";
 import blueprintsLibrary from "./blueprints";
 import { resolverMap } from "./resolvermap";
-import { ref, watch } from "vue";
+import { onBeforeMount, ref, watch } from "vue";
 
 const editor = useEditor()();
 
-const eventsContext = {
-  editor,
-};
-
-editor.setEventsContext(eventsContext);
+onBeforeMount(() => {
+  if (!editor.hasNodes) {
+    editor.setNodes([
+      {
+        componentName: "CraftCanvas",
+        props: {
+          component: "CraftComponentSimpleContainer",
+        },
+        children: [
+          {
+            label: "Text",
+            componentName: "CraftComponentSimpleText",
+            props: {
+              content: "@versa-stack/v-craft",
+              componentName: "h1",
+            },
+            children: [],
+            uuid: "29780b04-d063-4eea-b397-3e1443589d7e",
+            parentUuid: "d634ccb7-6b55-4787-80b2-2e922c9cb798",
+          },
+          {
+            label: "Text",
+            componentName: "CraftComponentSimpleText",
+            props: {
+              content: "hide",
+              componentName: "p",
+            },
+            children: [],
+            uuid: "07a03a93-269d-4539-bf47-05487ab7ed3b",
+            parentUuid: "d634ccb7-6b55-4787-80b2-2e922c9cb798",
+            events: {
+              click:
+                'const container = ctx.editor.nodeMap.get("bd5a073e-41b0-4781-99cb-6825ebdb5a2c");\n\nctx.editor.toggleNodeVisibility(container);\n\ncraftNode.props.content = container.visible === undefined || container.visible === true ? "hide" : "show";',
+            },
+          },
+          {
+            label: "Container with Text",
+            componentName: "CraftCanvas",
+            props: {
+              component: "CraftComponentSimpleContainer",
+            },
+            children: [
+              {
+                componentName: "CraftComponentSimpleText",
+                props: {
+                  content:
+                    "This content can be hidden with the CraftComponentSimpleText above. It used the onClick event to hide and show the CraftComponentSimpleContainer of this text.",
+                  componentName: "blockquote",
+                },
+                uuid: "c0e03a34-b46b-466b-b607-820e5571bf7e",
+                parentUuid: "bd5a073e-41b0-4781-99cb-6825ebdb5a2c",
+                children: [],
+              },
+            ],
+            uuid: "bd5a073e-41b0-4781-99cb-6825ebdb5a2c",
+            parentUuid: "d634ccb7-6b55-4787-80b2-2e922c9cb798",
+            visible: true,
+          },
+        ],
+        parentUuid: null,
+        uuid: "d634ccb7-6b55-4787-80b2-2e922c9cb798",
+        visible: true,
+      },
+    ]);
+  }
+});
 
 const previewContent = ref(false);
 
--- docs/editor.md
+++ docs/editor.md
@@ -1,7 +1,29 @@
-<script setup>
+<script lang="ts" setup>
 import Editor from "./components/demo/editor-app/editor.vue"
+import { useEditor } from "@versa-stack/v-craft"
+import { watch, ref, onMounted } from "vue"
+import {storeToRefs} from "pinia"
+
+const nodeTreeJson = ref("")
+const editor = useEditor()();
+const { nodeMap } = storeToRefs(editor);
+
+onMounted(() => {
+  nodeTreeJson.value = JSON.stringify(editor.nodeTree, null, 2);
+})
+
+watch(() => editor.nodeTree, (nt) => {
+  nodeTreeJson.value = JSON.stringify(nt, null, 2);
+}, {
+  deep: true
+})
+
 </script>
 
 <DemoContainer>
   <Editor />
 </DemoContainer>
+
+::: details Show editor content
+<pre class="w-full overflow-auto">{{ nodeTreeJson }}</pre>
+:::
